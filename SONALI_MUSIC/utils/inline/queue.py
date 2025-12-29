16abf2e3095f1090ab10cd59e4095e892b4b66da
Gyanesh Patel <gyaneshpatel605@gmail.com>
2025-12-30 01:42:53 +0530

    Update queue.py

--- SONALI_MUSIC/utils/inline/queue.py
+++ SONALI_MUSIC/utils/inline/queue.py
@@ -78,7 +78,7 @@ def aq_markup(_, chat_id):
                 text="• ᴄʜαɴɴᴇʟ 📢 •", url=f"https://t.me/aboutt_toxic"
             ),
             InlineKeyboardButton(
-                text="• ɢʀσυρ ✨ •", url=f"https://t.me/+vRWRO9arKIw1ZTM1"
+                text="• ɢʀσυρ ✨ •", url=f"https://t.me/+mr41Uo_5COViNGM1"
             ),
         ],
       ]
